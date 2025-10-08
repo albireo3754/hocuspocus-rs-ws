c9db5d8db6648bfade146c354af4179ab2f141f5
alibreo3754 <starpro123@naver.com>
2025-10-08 16:11:27 +0900

    refactor: example -> exampels/example

--- examples/example/src/main.rs
+++ examples/example/src/main.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 use hocuspocus_rs_ws::hocuspocus::Server;
 use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
 
     let port = 3000;
     info!("Launching Hocuspocus WebSocket server on 0.0.0.0:{}", port);
